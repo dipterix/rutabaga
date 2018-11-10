9352a37d93c1c851142dc6f8289e8a93b8689622
dipterix <dipterix.wang@gmail.com>
2018-11-09 18:30:21 -0600

    Added cat2 - colored cat Added generics %&% Added check installed packages

---
+++ R/utils_packages.R
@@ -0,0 +1,19 @@
+#' Check if packages are installed, returns missing packages
+#' @param pkgs vector of packages to install
+#' @param libs paths of libraries
+#' @param auto_install automatically install packages if missing
+#' @param ... other params for \code{install.packages}
+#' @importFrom utils install.packages
+#' @export
+check_installed_packages <- function(pkgs, libs = base::.libPaths(), auto_install = FALSE, ...){
+  instp = utils::installed.packages(lib.loc = libs)[,1]
+  pkgs = pkgs[!pkgs %in% instp]
+  if(auto_install){
+    cat2('Installing packages:', paste0('[', pkgs, ']', collapse = ', '), level = 'INFO')
+    do.call('install.packages', c(
+      list(pkgs = pkgs),
+      list(...)
+    ))
+  }
+  return(pkgs)
+}
